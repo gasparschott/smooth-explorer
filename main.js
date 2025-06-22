485d2963691b562d7ceff19a4439d147d147b316
gasparschott <mshroud@protonmail.com>
2025-06-22 13:10:08 -0700

    1.0.8

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 'use strict';
 
 let obsidian = require('obsidian');
-
+let DEFAULT_SETTINGS = {
+	'reveal_active_file_in_explorer': 		true
+}
 class SmoothExplorer extends obsidian.Plugin {
     async onload() {
 		// console.log('Loading the Smooth Explorer plugin.');
-		// await this.loadSettings();
+		await this.loadSettings();
+		this.addSettingTab(new SmoothExplorerSettings(this.app, this));
 		const workspace = this.app.workspace;
 		const getFileExplorers = () => workspace.getLeavesOfType('file-explorer');
 		const fileExplorerArrowNavigation = (e) => {
@@ -37,7 +40,7 @@ class SmoothExplorer extends obsidian.Plugin {
 							case workspace.getActiveFileView()?.leaf.pinned === true:														// active leaf is pinned
 							case workspace.getActiveFileView()?.leaf?.containerEl.closest('.mod-root') === null:							// active leaf in sidebar
 							case workspace.getActiveFileView(obsidian.FileView) === null:													// active leaf is empty
-								workspace.getLeaf().openFile(index,{active:true});													break;	// open new leaf
+								workspace.getLeaf().openFile(index,{active:false});													break;	// open new leaf
 							default:
 								workspace.getActiveFileView(obsidian.FileView)?.leaf?.openFile(index,{active:false});						// else open file in active leaf
 						}
@@ -56,7 +59,7 @@ class SmoothExplorer extends obsidian.Plugin {
 						case !e.shiftKey && !dupe: 																							// arrowKey only and no dupe
 							workspace.getActiveFileView(obsidian.FileView)?.leaf?.openFile(focused_file,{active:false}); 			break;	// open item in recently active tab
 						case dupe !== undefined:
-							workspace.setActiveLeaf(dupe,{focus:false});																break;	// focus dupe leaf
+							workspace.setActiveLeaf(dupe,{focus:false});															break;	// focus dupe leaf
 					}
 					workspace.setActiveLeaf(tree.leaf,{focus:false});																		// refocus file explorer
 					break;
@@ -74,6 +77,19 @@ class SmoothExplorer extends obsidian.Plugin {
 				fileExplorerArrowNavigation(e);
 			}
 		});	
+		this.registerEvent(
+			this.app.workspace.on('active-leaf-change', (e) => {
+				if ( workspace.getActiveViewOfType(obsidian.View).getViewType() === 'file-explorer' ) {
+					if ( this.settings.reveal_active_file_in_explorer === false ) { 
+						return; 
+					} else { 
+						this.app.commands.executeCommandById('file-explorer:reveal-active-file'); 
+					}			// reveal active file on file explorer becoming active
+					let tree = workspace.getLeavesOfType('file-explorer')[0].view.tree;
+					tree.setFocusedItem(tree?.view?.activeDom);												// focus the active file explorer item
+				}
+			})
+		);
 	} 
     // end onload
     // on plugin unload
@@ -81,12 +97,29 @@ class SmoothExplorer extends obsidian.Plugin {
 		// console.log('Unloading the Smooth Explorer plugin.');
     // }
 	// load settings
-    // async loadSettings() {
-		// this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-    // }
+    async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+    }
     // save settings
-    // async saveSettings() {
-		// await this.saveData(this.settings);
-    // }
+    async saveSettings() {
+		await this.saveData(this.settings);
+    }
+}
+let SmoothExplorerSettings = class extends obsidian.PluginSettingTab {
+	constructor(app, plugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+	display() {
+		const { containerEl } = this;
+		containerEl.empty();
+        containerEl.createEl("h1", {}, (el) => {el.innerHTML = 'Smooth Explorer'; });
+		new obsidian.Setting(containerEl).setName('Disable reveal active file in File Explorer').setDesc('Prevents revealing the active item when the File Explorer becomes active.')
+			.addToggle( A => A.setValue(this.plugin.settings.reveal_active_file_in_explorer)
+			.onChange(async (value) => {
+				this.plugin.settings.reveal_active_file_in_explorer = value;
+				await this.plugin.saveSettings();
+		}));
+	}
 }
 module.exports = SmoothExplorer;
