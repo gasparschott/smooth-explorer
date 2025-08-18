bb5bb2c58943e4f79b877e00b6074f7d4f58639c
gasparschott <mshroud@protonmail.com>
2025-08-17 21:03:13 -0700

    1.0.9

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 
 let obsidian = require('obsidian');
 let DEFAULT_SETTINGS = {
-	'disable_reveal_active_file_in_explorer': 		false
+	'disable_reveal_active_file_in_explorer': 		false,
+	'prefer_index_base_instead_of_md': 				false	
 }
 class SmoothExplorer extends obsidian.Plugin {
     async onload() {
@@ -20,9 +21,13 @@ class SmoothExplorer extends obsidian.Plugin {
 			let focused_item = tree.focusedItem, focused_file = focused_item?.file;
 			let select_this = ( focused_item ? focused_item : active_dom ? active_dom : null );
 			let dupe = workspace.getMostRecentLeaf().parent.children.find( leaf => leaf.view.file === focused_item?.file );
+			let index_type = new RegExp (( this.settings.prefer_index_base_instead_of_md === true ? "base" : "md" ),'i');
 			tree.clearSelectedDoms();
 			if ( focused_item === null && /ArrowUp|ArrowDown/.test(e.key) && !e.altKey ) { 													// focus items @ initial startup with first arrow keypress
-				active_dom !== null ? tree.setFocusedItem(active_dom) : e.key === 'ArrowDown' ? tree.setFocusedItem(tree.root.vChildren.first()) : e.key === 'ArrowUp' ? tree.setFocusedItem(tree.root.vChildren.last()) : null;
+				active_dom !== null ? tree.setFocusedItem(active_dom) 
+					: e.key === 'ArrowDown' ? tree.setFocusedItem(tree.root.vChildren.first()) 
+					: e.key === 'ArrowUp' ? tree.setFocusedItem(tree.root.vChildren.last()) 
+					: null;
 			}
 			switch(true) {
 				case !select_this: case !focused_item:																				break;
@@ -34,8 +39,17 @@ class SmoothExplorer extends obsidian.Plugin {
 					tree.selectItem(tree.focusedItem);																				return;
 				case this.app.vault.getFolderByPath(select_this.file.path) instanceof obsidian.TFolder:										// directories
 					tree.setFocusedItem(select_this,{scrollIntoView:true});
-					index = tree.focusedItem?.file?.children?.find( item => ( (/index/.test(item.basename) || item.basename === item.parent.name) && /md/.test(item.extension) ) ); // find index file
-					if (index) { 
+					index = tree.focusedItem?.file?.children?.find(																			// find index file
+						( item ) => {
+							if ( (/index/.test(item.basename) || item.basename === item.parent.name) ) {
+								switch(true) {
+									case index_type.test(item.extension) && this.settings.prefer_index_base_instead_of_md:			return true;
+									case index_type.test(item.extension) && /md/.test(item.extension):								return true;
+									case index_type.test(item.extension) && /base/.test(item.extension):							return true;
+								}
+							}
+					});
+					if (index) {																											// if index found...
 						switch(true) {
 							case workspace.getActiveFileView()?.leaf.pinned === true:														// active leaf is pinned
 							case workspace.getActiveFileView()?.leaf?.containerEl.closest('.mod-root') === null:							// active leaf in sidebar
@@ -44,6 +58,8 @@ class SmoothExplorer extends obsidian.Plugin {
 							default:
 								workspace.getActiveFileView(obsidian.FileView)?.leaf?.openFile(index,{active:false});						// else open file in active leaf
 						}
+					} else { 
+								workspace.getActiveFileView(obsidian.FileView)?.leaf?.detach();												// ...else close active file
 					};																												break;
 				case this.app.vault.getFileByPath(focused_item.file.path) instanceof obsidian.TFile:
 					switch(true) {
@@ -86,7 +102,7 @@ class SmoothExplorer extends obsidian.Plugin {
 						this.app.commands.executeCommandById('file-explorer:reveal-active-file'); 			// reveal active file on file explorer becoming active
 					}
 					let tree = workspace.getLeavesOfType('file-explorer')[0].view.tree;
-					tree.setFocusedItem(tree?.view?.activeDom);												// focus the active file explorer item
+					tree?.setFocusedItem(tree?.view?.activeDom);												// focus the active file explorer item
 				}
 			})
 		);
@@ -118,6 +134,12 @@ let SmoothExplorerSettings = class extends obsidian.PluginSettingTab {
 			.addToggle( A => A.setValue(this.plugin.settings.disable_reveal_active_file_in_explorer)
 			.onChange(async (value) => {
 				this.plugin.settings.disable_reveal_active_file_in_explorer = value;
+				await this.plugin.saveSettings();
+		}));
+		new obsidian.Setting(containerEl).setName('Prefer index.base to index.md').setDesc('When a File Explorer folder is selected, show index.base instead of index.md (if both exist). Enabling this will also prefer a .base file named after the enclosing folder to a similarly-named .md file (again, if both exist). If only one index file is found, it will be shown whether it is a .base or an .md file.')
+			.addToggle( A => A.setValue(this.plugin.settings.prefer_index_base_instead_of_md)
+			.onChange(async (value) => {
+				this.plugin.settings.prefer_index_base_instead_of_md = value;
 				await this.plugin.saveSettings();
 		}));
 	}
